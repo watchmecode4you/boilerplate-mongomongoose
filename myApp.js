1aa774ca89f066b960213b04d4f59559f64ddb49
joseph <josephmaary@outlook.com>
2022-05-07 17:36:38 +0300

    added nodemon for better code changes handling & roubleshooting the error i'm having in heroku cli

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose")
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(process.env.MONGO_URI.toString())
+mongoose.connect(process.env.MONGO_URI.toString(), { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
