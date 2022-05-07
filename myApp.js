3a2c53058c88409700d036e2c625c363aa93e235
joseph <josephmaary@outlook.com>
2022-05-07 17:50:21 +0300

    removed some stuff

--- myApp.js
+++ myApp.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose = require("mongoose")
-console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
