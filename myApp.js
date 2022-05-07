f04af70b8f3a5478a4d76beeeb7119e44eb946f4
joseph <josephmaary@outlook.com>
2022-05-07 17:44:53 +0300

    removed the console.log function and the toString method from myapp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose")
-console.log(process.env.MONGO_URI.toString())
-mongoose.connect(process.env.MONGO_URI.toString(), { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
